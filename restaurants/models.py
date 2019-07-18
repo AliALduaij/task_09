717e12faccc023cc6a6c5e197fdb72f1af93cb7e
MAC <mac@MACs-MacBook-Pro.local>
2019-07-18 17:50:12 +0300

    i always forget to push my projects

--- restaurants/models.py
+++ restaurants/models.py
@@ -9,3 +9,7 @@ class Restaurant(models.Model):
 
     def __str__(self):
     	return self.name
+
+class User(models.Model):
+	username=models.CharField(max_length=30)
+	password=models.CharField(max_length=30)
